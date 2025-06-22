3f0f7b9080fd27b9967653c4754df0296d0e4984
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-06-22 19:10:33 +0100

    update thunkTasks.ts

--- store/async/thunkTasks.ts
+++ store/async/thunkTasks.ts
@@ -1,31 +1,28 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { monday } from "db/schema";
 import useDb from "hooks/useDb";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+export const getThunkTasks = createAsyncThunk("getTasks", async (_, thunkAPI) => {
+  const { rejectWithValue } = thunkAPI
 
-export const getThunkTasks = createAsyncThunk("getTasks",async(_,thunkAPI)=>{
-    const {rejectWithValue}= thunkAPI
-const db = useDb()
-
-    try {
-      const data = await db.select().from(monday)
-      console.log("fff");
-      return data
-    } catch (error) {
-        console.log(rejectWithValue(error));  
-    }
+  try {
+    await AsyncStorage.getItem('tasks');
+  } catch (err) {
+    console.log(rejectWithValue(err));
+  }
 })
 
 
-export const insertInThunk = createAsyncThunk("insertTask",async (data:string,thunkAPI)=>{
-  const {rejectWithValue} = thunkAPI;
-const db = useDb()
+export const insertInThunk = createAsyncThunk("insertTask", async (data: string, thunkAPI) => {
+  const { rejectWithValue } = thunkAPI;
+  const db = useDb()
 
   try {
     const res = await db.insert(monday).values(
-        {
-          task: data,
-        },
-      )
+      {
+        task: data,
+      },
+    )
     return res
   } catch (error) {
     console.log(rejectWithValue(error));
